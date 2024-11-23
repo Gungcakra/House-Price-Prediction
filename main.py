52fbfa39d0806f97a0b0a940397b5d117e4eab2c
Gungcakra <agungcakra888@gmail.com>
2024-11-23 13:38:20 +0800

    update

--- main.py
+++ main.py
@@ -10,15 +10,12 @@ y = data['price']
 # Model
 model = LinearRegression()
 
-# Latih model
 model.fit(X, y)
 
-# Menggunakan DataFrame untuk input prediksi, agar memiliki nama kolom yang sesuai
 house_size = float(input("Input house size (m²): "))
 input_data = pd.DataFrame([[house_size]], columns=['size'])
 
 # Prediksi harga rumah berdasarkan ukuran
 predicted_price = model.predict(input_data)
 
-# Menampilkan hasil prediksi
 print(f'The predicted price of a house with a size of {house_size} m² is: Rp {predicted_price[0]:,.2f}')
